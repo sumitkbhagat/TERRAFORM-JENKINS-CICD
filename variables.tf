753a596d447be2bf0cb58037242b215c99930a75
Sumit Kumar Bhagat <83488179+sumitkbhagat@users.noreply.github.com>
2023-10-02 19:32:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "ap-southeast-2"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "sydneykey"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "ajaykumar-yegireddi-cloud"
+  default     = "sumitkumar-cloud"
 }
