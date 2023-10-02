585ac2b8e1af78483f70c201124f4704e1775550
Sumit Kumar Bhagat <83488179+sumitkbhagat@users.noreply.github.com>
2023-10-02 19:55:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "sumit-devops"
+    bucket         = "sumit-bhagat"
     key            = "my-terraform-environment/main"
     region         = "ap-southeast-2"
     dynamodb_table = "sumit-dynamo-db-table"
